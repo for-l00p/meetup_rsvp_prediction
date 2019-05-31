986d350627f43af7291e6928932bab1af3cb86ac
Grace Jeong <grace.jeong@flatironschool.com>
2019-05-30 21:30:39 -0400

    add file

---
+++ All_2018/datacleaning_functions.py
@@ -0,0 +1,45 @@
+# Helper functions for cleaning all 2018 events data
+
+
+def load_data():
+    """
+    This function loads all monthly events, then merges into one variable
+    and returns the data in a dataframe
+    """
+    with open('monthly_events_2018/sepoct_events.pkl', 'rb') as f:
+        sepoct_events = pickle.load(f)
+
+    with open('monthly_events_2018/nov_events.pkl', 'rb') as f:
+        nov_events = pickle.load(f)
+
+    with open('monthly_events_2018/jan_events.pkl', 'rb') as f:
+        jan_events = pickle.load(f)
+
+    with open('monthly_events_2018/feb_events.pkl', 'rb') as f:
+        feb_events = pickle.load(f)
+
+    with open('monthly_events_2018/mar_events.pkl', 'rb') as f:
+        mar_events = pickle.load(f)
+
+    with open('monthly_events_2018/apr_events.pkl', 'rb') as f:
+        apr_events = pickle.load(f)
+
+    with open('monthly_events_2018/may_events.pkl', 'rb') as f:
+        may_events = pickle.load(f)
+
+    with open('monthly_events_2018/jun_events.pkl', 'rb') as f:
+        jun_events = pickle.load(f)
+
+    with open('monthly_events_2018/jul_events.pkl', 'rb') as f:
+        jul_events = pickle.load(f)
+
+    with open('monthly_events_2018/aug_events.pkl', 'rb') as f:
+        aug_events = pickle.load(f)
+
+    with open('monthly_events_2018/dec_events.pkl', 'rb') as f:
+        dec_events = pickle.load(f)
+
+    raw_2018 = sepoct_events+nov_events+jan_events+feb_events+mar_events + \
+        apr_events+may_events+jun_events+jul_events+aug_events+dec_events
+
+    return pd.DataFrame(raw_2018)
